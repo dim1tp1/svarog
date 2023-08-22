ca3ed7d9aadb33942baba9ef4845c273116597a4
dim1tp1 <dim1tp1@proton.me>
2023-08-22 16:40:30 +0800

    modify makefile; delete java codes

--- lx_grpc/protoc.mk
+++ lx_grpc/protoc.mk
@@ -1,11 +1,13 @@
 .PHONY: all
 
-GO_OUT = ../luban_manager/monitiel
+all: rust
 
-all:
+rust:
 	cargo build --release --manifest-path=protoc/Cargo.toml
 	rsync -avP protoc/target/release/luban_protoc ./
 	./luban_protoc
+
+java:
 	rm -rf src_java
 	mkdir src_java
 	protoc --java_out=src_java --grpc-java_out=src_java --proto_path=proto proto/*.proto
